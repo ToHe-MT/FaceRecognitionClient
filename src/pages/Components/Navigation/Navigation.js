8a93f4d8e7de578b2e0e0ad76031724eb1b309b3
Theo Millard Tirtawinata <theo.millard@gmail.com>
2023-11-30 13:13:38 +0700

    removing things

--- src/pages/Components/Navigation/Navigation.js
+++ src/pages/Components/Navigation/Navigation.js
@@ -12,13 +12,13 @@ const Navigation = ({ onRouteChange, isSignedIn, route, emptyUser }) => {
         return (
             <nav className="flex flex-row-reverse pt-8 pr-8" >
                 {route === 'register' ?
-                    <p onClick={() => onRouteChange("register")} className="px-4 py-1 rounded-lg text-slate-600 duration200 hover:underline cursor-pointer">
-                        Register
-                        </p>
-                    :
                     <p onClick={() => onRouteChange("signin")} className="px-4 py-1 rounded-lg text-slate-600 duration-200 hover:underline cursor-pointer">
                         Sign In
-                        </p>
+                    </p>
+                    :
+                    <p onClick={() => onRouteChange("register")} className="px-4 py-1 rounded-lg text-slate-600 duration200 hover:underline cursor-pointer">
+                        Register
+                    </p>
                 }
             </nav>
         )
